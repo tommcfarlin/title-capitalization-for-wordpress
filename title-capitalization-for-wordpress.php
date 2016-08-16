edb29e41e91db150585333f8382d1a3abc245e78
Tom McFarlin <tom@tommcfarlin.com>
2016-08-16 17:33:21 -0400

    Updates to the functionality of the bootstrap.
    - Updates the docblocks
    - Updates version numbers
    - Updates the year timestamp (2 years old — yeesh!)
    - Updates how files are loaded
    - Updates the package
    - Uses a different hook to start the plugin

--- title-capitalization-for-wordpress.php
+++ title-capitalization-for-wordpress.php
@@ -2,23 +2,24 @@
 /**
  * Title Capitalization For WordPress
  *
- * Title Capitalization For WordPress is a plugin that converts post titles and post content
- * heading elements (that is, `h1`, `h2`, ..., `h6`) into properly capitalized headings.
+ * Title Capitalization For WordPress is a plugin that converts post titles and
+ * post content heading elements (that is, `h1`, `h2`, ..., `h6`) into properly
+ * capitalized headings.
  *
- * This is based on the work of John Gruber, David Gouch, and Kroc Camen that has been adapted
- * into a WordPress plugin.
+ * This is based on the work of John Gruber, David Gouch, and Kroc Camen that
+ * has been adapted into a WordPress plugin.
  *
- * @package   TitleCapitalizer
+ * @package   Title_Capitalization
  * @author    Tom McFarlin <tom@tommcfarlin.com>
  * @license   GPL-2.0+
- * @link      http://tommcfarlin.com/title-capitalization-for-wordpress/
- * @copyright 2014 Tom McFarlin
+ * @link      https://tommcfarlin.com/title-capitalization-for-wordpress/
+ * @copyright 2014 - 2016 Tom McFarlin
  *
  * @wordpress-plugin
  * Plugin Name:       Title Capitalization For WordPress
  * Plugin URI:        http://tommcfarlin.com/title-capitalization-for-wordpress/
  * Description:       Converts post and page titles and post content headings into proper capitalization.
- * Version:           1.2.0
+ * Version:           1.3.0
  * Author:            Tom McFarlin
  * Author URI:        http://tommcfarlin.com/
  * License:           GPL-2.0+
@@ -26,8 +27,6 @@
  * Domain Path:       /languages
  * Text Domain:       title-capitalization-for-wordpress
  * Network:           false
- * GitHub Plugin URI: tommcfarlin/title-capitalization-for-wordpress
- * GitHub Branch:     master
  */
 
 // If this file is called directly, abort.
@@ -35,27 +34,26 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// Only run this plugin if we're in the dashboard.
-if ( is_admin() ) {
+// Load the Title Case Library (if it has not already been loaded).
+if ( ! class_exists( 'TitleCase' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/vendor/class-title-case.php' );
+}
 
-	/**
-	 * Load the Title Case Library (if it has not already been loaded)
-	 */
-	if ( ! class_exists( 'TitleCase' ) ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'includes/vendor/class-title-case.php' );
-	}
+// Load the class responsible for defining actions and their callbacks.
+require_once( plugin_dir_path( __FILE__ ) . 'admin/class-title-capitalization-loader.php' );
 
-	/**
-	 * Load the class responsible for defining actions and their callbacks.
-	 */
-	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-title-capitalizer-loader.php' );
+// Load the core plugin file responsible for processing titles and headings.
+require_once( plugin_dir_path( __FILE__ ) . 'admin/class-title-capitalizer.php' );
 
-	/**
-	 * Load the core plugin file responsible for processing titles and headings.
-	 */
-	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-title-capitalizer.php' );
+// Opting to use `admin_init` here since it's administration-screen specified.
+add_action( 'admin_init', 'tm_title_capitalization_start' );
+/**
+ * Start the machine.
+ * https://www.youtube.com/watch?v=ysoMOefPyRs
+ */
+function tm_title_capitalization_start() {
 
-	$title_caps_loader = new Title_Capitalizer_Loader();
-	$title_caps_loader->run( new Title_Capitalizer( new TitleCase() ) );
+	$plugin = new Title_Capitalization_Loader();
+	$plugin->run( new Title_Capitalizer( new TitleCase() ) );
 
 }
