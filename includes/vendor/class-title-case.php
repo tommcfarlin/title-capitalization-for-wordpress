736d4330e0912754088ba8d7fd59c38ae1f57610
Andy Fragen <andy@thefragens.com>
2016-08-19 19:24:33 -0700

    refactor of title-case lib
    removed nested ternary equations

--- includes/vendor/class-title-case.php
+++ includes/vendor/class-title-case.php
@@ -2,59 +2,77 @@
 //original Title Case script © John Gruber <daringfireball.net>
 //javascript port © David Gouch <individed.com>
 //PHP port of the above by Kroc Camen <camendesign.com>
+//refactor and mods for contractions and markdown headers by Andy Fragen <andy@thefragens.com>
 
 class TitleCase {
 
-	public function toTitleCase ($title) {
+	public function toTitleCase( $title ) {
 		//remove HTML, storing it for later
 		//       HTML elements to ignore    | tags  | entities
 		$regx = '/<(code|var)[^>]*>.*?<\/\1>|<[^>]+>|&\S+;|(#+ )/';
-		preg_match_all ($regx, $title, $html, PREG_OFFSET_CAPTURE);
-		$title = preg_replace ($regx, '', $title);
+		preg_match_all( $regx, $title, $html, PREG_OFFSET_CAPTURE );
+		$title = preg_replace( $regx, '', $title );
 
 		//find each word (including punctuation attached)
-		preg_match_all ('/[\w\p{L}&`\'‘’"“\.@:\/\{\(\[<>_]+-? */u', $title, $m1, PREG_OFFSET_CAPTURE);
-		foreach ($m1[0] as &$m2) {
+		preg_match_all( '/[\w\p{L}&`\'‘’"“\.@:\/\{\(\[<>_]+-? */u', $title, $m1, PREG_OFFSET_CAPTURE );
+
+		foreach ( $m1[0] as &$m2 ) {
+
 			//shorthand these- "match" and "index"
-			list ($m, $i) = $m2;
+			list ( $m, $i ) = $m2;
 
 			//correct offsets for multi-byte characters (`PREG_OFFSET_CAPTURE` returns *byte*-offset)
 			//we fix this by recounting the text before the offset using multi-byte aware `strlen`
-			$i = mb_strlen (substr ($title, 0, $i), 'UTF-8');
+			$i = mb_strlen( substr( $title, 0, $i ), 'UTF-8' );
 
 			//find words that should always be lowercase…
 			//(never on the first word, and never if preceded by a colon)
-			$m = $i>0 && mb_substr ($title, max (0, $i-2), 1, 'UTF-8') !== ':' &&
-				!preg_match ('/[\x{2014}\x{2013}] ?/u', mb_substr ($title, max (0, $i-2), 2, 'UTF-8')) &&
-				 preg_match ('/^(a(nd?|s|t)?|b(ut|y)|en|for|i[fn]|o[fnr]|t(he|o)|vs?\.?|via)[ \-]/i', $m) ||
-			     preg_match( '/\'(s|t|ll|re|ve|d|m|em)/i', $m ) // Don't capitalize contractions
-			?	//…and convert them to lowercase
-				mb_strtolower ($m, 'UTF-8')
+			if (
+				$i > 0 && mb_substr( $title, max( 0, $i - 2 ), 1, 'UTF-8' ) !== ':' &&
+				! preg_match( '/[\x{2014}\x{2013}] ?/u', mb_substr( $title, max( 0, $i - 2 ), 2, 'UTF-8' ) ) &&
+				preg_match( '/^(a(nd?|s|t)?|b(ut|y)|en|for|i[fn]|o[fnr]|t(he|o)|vs?\.?|via)[ \-]/i', $m ) ||
 
-			//else:	brackets and other wrappers
-			: (	preg_match ('/[\'"_{(\[‘“]/u', mb_substr ($title, max (0, $i-1), 3, 'UTF-8'))
-			?	//convert first letter within wrapper to uppercase
-				mb_substr ($m, 0, 1, 'UTF-8').
-				mb_strtoupper (mb_substr ($m, 1, 1, 'UTF-8'), 'UTF-8').
-				mb_substr ($m, 2, mb_strlen ($m, 'UTF-8')-2, 'UTF-8')
+				// Don't capitalize contractions
+				preg_match( '/\'(s|t|ll|re|ve|d|m|em)/i', $m )
+			) {
+				//…and convert them to lowercase
+				$m = mb_strtolower( $m, 'UTF-8' );
 
-			//else:	do not uppercase these cases
-			: (	preg_match ('/[\])}]/', mb_substr ($title, max (0, $i-1), 3, 'UTF-8')) ||
-				preg_match ('/[A-Z]+|&|\w+[._]\w+/u', mb_substr ($m, 1, mb_strlen ($m, 'UTF-8')-1, 'UTF-8'))
-			?	$m
+			} elseif ( //else:	brackets and other wrappers
+				preg_match( '/[\'"_{(\[‘“]/u', mb_substr( $title, max( 0, $i - 1 ), 3, 'UTF-8' ) )
+			) {
+				//convert first letter within wrapper to uppercase
+				$m = mb_substr( $m, 0, 1, 'UTF-8' );
+
+				//if single letter word, capitalize it
+				if ( 1 === mb_strlen( $m, 'UTF-8' ) ) {
+					$m = mb_strtoupper( $m, 'UTF-8' );
+				} else {
+					$m .= mb_strtoupper( mb_substr( $m, 1, 1, 'UTF-8' ), 'UTF-8' ) .
+					      mb_substr( $m, 2, mb_strlen( $m, 'UTF-8' ) - 2, 'UTF-8' );
+				}
+			} elseif (
+				//else:	do not uppercase these cases
+				preg_match( '/[\])}]/', mb_substr( $title, max( 0, $i - 1 ), 3, 'UTF-8' ) ) ||
+				preg_match( '/[A-Z]+|&|\w+[._]\w+/u', mb_substr( $m, 1, mb_strlen( $m, 'UTF-8' ) - 1, 'UTF-8' ) )
+			) {
+				$m = $m;
+			} else {
 				//if all else fails, then no more fringe-cases; uppercase the word
-			:	mb_strtoupper (mb_substr ($m, 0, 1, 'UTF-8'), 'UTF-8').
-				mb_substr ($m, 1, mb_strlen ($m, 'UTF-8'), 'UTF-8')
-			));
+				$m = mb_strtoupper( mb_substr( $m, 0, 1, 'UTF-8' ), 'UTF-8' ) .
+				     mb_substr( $m, 1, mb_strlen( $m, 'UTF-8' ), 'UTF-8' );
+			}
 
-			//resplice the title with the change (`substr_replace` is not multi-byte aware)
-			$title = mb_substr ($title, 0, $i, 'UTF-8').$m.
-				 mb_substr ($title, $i+mb_strlen ($m, 'UTF-8'), mb_strlen ($title, 'UTF-8'), 'UTF-8')
-			;
+			//re-splice the title with the change (`substr_replace` is not multi-byte aware)
+			$title = mb_substr( $title, 0, $i, 'UTF-8' ) . $m .
+			         mb_substr( $title, $i + mb_strlen( $m, 'UTF-8' ), mb_strlen( $title, 'UTF-8' ), 'UTF-8' );
 		}
 
 		//restore the HTML
-		foreach ($html[0] as &$tag) $title = substr_replace ($title, $tag[0], $tag[1], 0);
+		foreach ( $html[0] as &$tag ) {
+			$title = substr_replace( $title, $tag[0], $tag[1], 0 );
+		}
+
 		return $title;
 	}
 
